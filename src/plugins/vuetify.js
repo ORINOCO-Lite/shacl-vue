0786fc806082fb32c18c9882a25d8996287a9092
Stephan Heunis <s.heunis@fz-juelich.de>
2026-02-05 16:09:53 +0100

    set vuetify theme default to system

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -14,6 +14,14 @@ import { createVuetify } from 'vuetify';
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
     theme: {
-        defaultTheme: 'light',
+        defaultTheme: 'system',
+        themes: {
+            light: {
+                dark: false,
+            },
+            dark: {
+                dark: true,
+            },
+        },
     },
 });
