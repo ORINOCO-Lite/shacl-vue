751c8340d1c0b177c9fd3ff0a606c575368ee623
Stephan Heunis <s.heunis@fz-juelich.de>
2026-02-24 23:03:17 +0100

    Introduce 'BackLinkViewer' component and associated config
    This new component allows viewing so-called backlinks for a record, i.e. other records
    that reference the current record via some property, directly or indirectly. Upon
    mounting, the component will first fetch all 'Thing' records (importantly: including
    subclasses) that somehow include the PID of the current record (via 'matching' parament)
    from the backend service. Determining which referencing records to show required a new
    utility function 'getReferencingRecords' which does the following:
    - get all quads with the current PID as object
    - if the quad has a named node as subject, save it as a referencing record
    - if the quad has a blank node as subject, get all quads with the blank node ID as object
    
    This means it recurses into the second level to find referencing records, and no further.
    This is a restriction that could be reconsidered and addressed in future, although local
    testing on actual data compliant to the xyzri schema has not shown this to be necessary.
    
    Saving a referencing record also includes saving the quad's predicate, which is taken as
    the referencing property, e.g. a 'Person' is referenced by a 'Dataset' via 'attributed_to'.
    This allows grouping referencing records by property in the 'BackLinkViewer' component.
    
    Lastly, a new config sub-option 'hideBackLinks' is introduces for the 'NodeShapeViewer's
    'component_config'. An example:
    
    component_config:
      NodeShapeViewer:
        hideBackLinks:
          - dlthings:AnnotationTag
          - xyzri:AgentRole
    
    'hideBackLinks' can be a boolean or array. It is set to 'true' by default, meaning that
    backlinks won't be displayed by default (i.e. when the option is not configured). If an
    array with class CURIEs is supplied, the back links will be displayed for records of all
    classes except for those included in the configured array.

---
+++ src/components/BackLinkViewer.vue
@@ -0,0 +1,48 @@
+<template>
+    <h4 v-if="Object.keys(refRecords).length" style="margin-bottom: 1em;" >Records referencing <em>{{props.record.displayLabel}}</em>:</h4>
+    <div v-for="(arr, key) in refRecords" style="margin-bottom: 0.5em;">
+        <em>via <strong>{{makeReadable(toCURIE(key, allPrefixes, 'parts').property)}}</strong></em>:
+        <span v-for="r in arr">
+            <br>&nbsp;-&nbsp;
+            <NamedNodeViewer
+                :textVal="r.record_id"
+                :prefLabel="getPrefLabel(r.quad.subject, rdfDS, allPrefixes)"
+                :displayLabel="getRecordDisplayLabel(r.quad.subject, rdfDS, allPrefixes, configVarsMain)"
+                :quad="r.quad"
+                :targetClass="r.class_iri"
+                :allowLink="true"
+            >
+            </NamedNodeViewer>
+        </span>
+    </div>
+</template>
+
+<script setup>
+
+import { inject, onMounted, ref } from 'vue';
+import { toCURIE, getReferencingRecords, getPrefLabel, getRecordDisplayLabel, makeReadable} from '@/modules/utils';
+
+const props = defineProps({
+    record: Object,
+});
+
+const emit = defineEmits(['has-referencing-records'])
+
+const fetchFromService = inject('fetchFromService')
+const allPrefixes = inject('allPrefixes')
+const rdfDS = inject('rdfDS')
+const configVarsMain = inject('configVarsMain')
+const refRecords = ref({});
+
+
+onMounted(async () => {
+    const arg = allPrefixes['dlthings'] + 'Thing';
+    await fetchFromService('get-paginated-records-constrained', arg, allPrefixes, props.record.title )
+    await fetchFromService('get-paginated-records-constrained', arg, allPrefixes, toCURIE(props.record.title, allPrefixes))
+    refRecords.value = getReferencingRecords(props.record.title, rdfDS.data.graph)
+    if (Object.keys(refRecords.value).length > 0) {
+        emit('has-referencing-records')
+    }
+})
+    
+</script>
