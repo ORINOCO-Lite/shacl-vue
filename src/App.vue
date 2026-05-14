cd6ad23d540e3253bdf314eb493c3cb2e3d50a4f
Stephan Heunis <s.heunis@fz-juelich.de>
2026-05-14 14:22:38 +0200

    use correct component instantiation, without assumiong a specified config url

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <v-app>
-        <!-- <component :is="appVariant" /> -->
-        <component :is="appVariant" :configUrl="confURL"></component>
+        <component :is="appVariant" />
+        <!-- <component :is="appVariant" :configUrl="confURL"></component> -->
     </v-app>
 </template>
 
