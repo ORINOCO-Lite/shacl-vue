c9d7e535a34dec42a9189472eae395c3a18fbd38
Stephan Heunis <s.heunis@fz-juelich.de>
2026-07-22 22:35:00 +0200

    Add failsafe for when updateNodeShape tries to update a non-existent nodeshape

--- src/composables/useShapes.js
+++ src/composables/useShapes.js
@@ -49,38 +49,43 @@ export function useShapes(config) {
     
     function updateNodeShape(newShapeIRI, newShapeObj, allPrefixes) {
         const targetNodeShapeIRI = toIRI(newShapeIRI, allPrefixes)
-        const targetNodeShapeProperties = shapesDS.data.nodeShapes[targetNodeShapeIRI]['properties']
-        for (const key of Object.keys(newShapeObj)) {
-            const keyIRI = toIRI(key, allPrefixes)
-            const value = newShapeObj[key];
-            if (keyIRI == SHACL.property.value) {
-                // if the key is "sh:property", we have to find the correct propertyShape per property
-                for (const prop of Object.keys(value)) {
-                    const pathIRI = toIRI(prop, allPrefixes)
-                    const propVal = value[prop]
-                    // console.log(`have to find property shape in array, where path `)
-                    const targetPropertyShape = findObjectByKey(targetNodeShapeProperties, SHACL.path.value, pathIRI)
-                    if (targetPropertyShape) {
-                        // update existing property shape with all new key-value pairs
-                        updatePropertyShape(targetPropertyShape, propVal, allPrefixes)
-                    } else {
-                        // create new property shape object, by copying
-                        const newPropShape = toRaw(propVal);
-                        for (const key of Object.keys(newPropShape)) {
-                            // cast boolean as string
-                            if (typeof newPropShape[key] == "boolean") {
-                                newPropShape[key] = `${newPropShape[key]}`
+
+        if (shapesDS.data.nodeShapes[targetNodeShapeIRI]) {
+            const targetNodeShapeProperties = shapesDS.data.nodeShapes[targetNodeShapeIRI]['properties']
+            for (const key of Object.keys(newShapeObj)) {
+                const keyIRI = toIRI(key, allPrefixes)
+                const value = newShapeObj[key];
+                if (keyIRI == SHACL.property.value) {
+                    // if the key is "sh:property", we have to find the correct propertyShape per property
+                    for (const prop of Object.keys(value)) {
+                        const pathIRI = toIRI(prop, allPrefixes)
+                        const propVal = value[prop]
+                        // console.log(`have to find property shape in array, where path `)
+                        const targetPropertyShape = findObjectByKey(targetNodeShapeProperties, SHACL.path.value, pathIRI)
+                        if (targetPropertyShape) {
+                            // update existing property shape with all new key-value pairs
+                            updatePropertyShape(targetPropertyShape, propVal, allPrefixes)
+                        } else {
+                            // create new property shape object, by copying
+                            const newPropShape = toRaw(propVal);
+                            for (const key of Object.keys(newPropShape)) {
+                                // cast boolean as string
+                                if (typeof newPropShape[key] == "boolean") {
+                                    newPropShape[key] = `${newPropShape[key]}`
+                                }
                             }
+                            // add path, because it is likely not specified from config (since it's not required)
+                            newPropShape[SHACL.path.value] = pathIRI;
+                            targetNodeShapeProperties.push(newPropShape)
                         }
-                        // add path, because it is likely not specified from config (since it's not required)
-                        newPropShape[SHACL.path.value] = pathIRI;
-                        targetNodeShapeProperties.push(newPropShape)
                     }
+                } else {
+                    // Assign value on the nodeShape key, as is (i.e. no validation of the value => could be problematic)
+                    shapesDS.data.nodeShapes[targetNodeShapeIRI][keyIRI] = value;
                 }
-            } else {
-                // Assign value on the nodeShape key, as is (i.e. no validation of the value => could be problematic)
-                shapesDS.data.nodeShapes[targetNodeShapeIRI][keyIRI] = value;
             }
+        } else {
+            console.log(`Trying to update non-existent nodeshape: ${targetNodeShapeIRI}. skipping...`)
         }
     }
 
